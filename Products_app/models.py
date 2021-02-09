38265c37b4b9b7e0f0a9e697a6835fe9f5f6c424
unknown <leusnazarii@gmail.com>
2021-02-09 19:51:43 +0200

    Added git

---
+++ Products_app/models.py
@@ -0,0 +1,132 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from .utilities import get_timestamp_path
+
+
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(default=True,
+                                       db_index=True,
+                                       verbose_name='Прошел активацию?')
+
+    class Meta(AbstractUser.Meta):
+        pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30,
+                            db_index=True,
+                            unique=True,
+                            verbose_name='Название')
+    order = models.SmallIntegerField(default=0,
+                                     db_index=True,
+                                     verbose_name='Порядок')
+    super_category = models.ForeignKey('SuperCategory',
+                                       on_delete=models.PROTECT,
+                                       null=True,
+                                       blank=True,
+                                       verbose_name='Надкатегория')
+
+
+class SuperCategoryManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_category__isnull=True)
+
+
+class SuperCategory(Category):
+    objects = SuperCategoryManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        proxy = True
+        ordering = ('order', 'name')
+        verbose_name = 'Надкатегория'
+        verbose_name_plural = 'Надкатегории'
+
+
+class SubCategoryManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_category__isnull=False)
+
+
+class SubCategory(Category):
+    objects = SubCategoryManager()
+
+    def __str__(self):
+        return f'{self.super_category.name} - {self.name}'
+
+    class Meta:
+        proxy = True
+        ordering = ('super_category__order',
+                    'super_category__name',
+                    'order',
+                    'name')
+        verbose_name = 'Подкатегория'
+        verbose_name_plural = 'Подкатегории'
+
+
+class Product(models.Model):
+    currency_list = (
+        (None, '------------------------------'),
+        ('грн.', 'UAH'),
+        ('долл.', 'USD'),
+        ('ев.', 'EUR'),
+    )
+    p_countries = (
+        (None, '------------------------------'),
+        ('UA', 'Украина'),
+        ('RU', 'Россия'),
+        ('PL', 'Польша'),
+        ('BL', 'Белорусь'),
+        ('CN', 'Китай'),
+    )
+
+    title = models.CharField(max_length=50, verbose_name='Название')
+    image = models.ImageField(blank=True, upload_to=get_timestamp_path, verbose_name='Изображение')
+    producing_country = models.CharField(null=True, max_length=24,choices=p_countries,
+                                         verbose_name='Страна производитель')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+    currency = models.CharField(null=True, max_length=8, choices=currency_list, verbose_name='Валюта')
+    description = models.TextField(max_length=256, verbose_name='Описание')
+    in_stock = models.BooleanField(default=True, verbose_name='В наличии')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата добавления')
+    category = models.ForeignKey(SubCategory, on_delete=models.PROTECT, verbose_name='Категория')
+
+    def delete(self, *args, **kwargs):
+        for i in self.additionalimages_set.all():
+            i.delete(*args, **kwargs)
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['-published']
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                verbose_name='Товар')
+    author = models.CharField(max_length=50, verbose_name='Автор')
+    text = models.TextField(verbose_name='Текст комментария')
+    published = models.DateTimeField(auto_now_add=True, db_index=True,
+                                     verbose_name='Опубликован')
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-published']
+
+
+class AdditionalImages(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Объявление')
+    image = models.ImageField(upload_to=get_timestamp_path, verbose_name='Изображение')
+
+    class Meta:
+        verbose_name = 'Дополнительное изображение'
+        verbose_name_plural = 'Дополнительные изображения'
+
---
+++ Products_app/views.py
@@ -0,0 +1,253 @@
+from django.urls import reverse_lazy
+from django.http import Http404
+from django.db.models import Q
+from django.template.loader import get_template
+from django.template import TemplateDoesNotExist
+from django.shortcuts import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import logout
+from django.contrib import messages
+from django.views.generic import ListView
+from django.views.generic import UpdateView
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import TemplateView
+from django.views.generic import DetailView
+from django.core.paginator import Paginator
+
+from .models import AdvUser
+from .models import Product
+from .models import AdditionalImages
+from .models import Category
+from .models import SubCategory
+from .models import Comment
+
+from .forms import ChangeUserInfoForm
+from .forms import RegisterUserForm
+from .forms import SearchForm
+from .forms import UserCommentForm
+
+from cart.forms import CartAddProductForm
+
+
+def other_page(request, page):
+    try:
+        template = get_template(page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+
+class RegisterUserView(CreateView):
+    model = AdvUser
+    template_name = 'register_user.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('products:reg_successful')
+
+
+class RegisterSuccessfulView(TemplateView):
+    template_name = 'register_successful.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+
+class PLoginView(LoginView):
+    template_name = 'login.html'
+    success_url = reverse_lazy('products:index')
+
+
+class PLogoutView(LoginRequiredMixin,
+                  LogoutView):
+    template_name = 'logout.html'
+
+
+class ChangeUserInfoView(SuccessMessageMixin,
+                         LoginRequiredMixin,
+                         UpdateView):
+    model = AdvUser
+    template_name = 'change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('products:profile')
+    success_message = 'Информация о пользователе изменена'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset,
+                                 pk=self.user_id)
+
+
+class PChangePasswordView(SuccessMessageMixin,
+                          LoginRequiredMixin,
+                          PasswordChangeView):
+    template_name = 'change_password.html'
+    success_url = reverse_lazy('products:profile')
+    success_message = 'Пароль успешно изменен'
+
+
+class PDeleteUserView(LoginRequiredMixin,
+                      DeleteView):
+    model = AdvUser
+    template_name = 'delete_user.html'
+    success_url = reverse_lazy('products:index')
+
+    def dispatch(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        messages.add_message(request, messages.SUCCESS,
+                             'Пользователь удален')
+        return super().post(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset,
+                                 pk=self.user_id)
+
+
+def search_keyword(request, products_obj):
+    keyword = ''
+    products = products_obj
+    if 'search_keyword' in request.GET:
+        keyword = request.GET['search_keyword']
+        q = Q(title__contains=keyword) | Q(description__contains=keyword)
+        products = products_obj.filter(q)
+    return {'keyword': keyword,
+            'products': products}
+
+
+def my_paginator(request, objects, items_in_page=1):
+    paginator = Paginator(objects, items_in_page)
+    if 'page' in request.GET:
+        page_number = request.GET['page']
+    else:
+        page_number = 1
+    return paginator.get_page(page_number)
+
+
+def index(request):
+    template = 'index.html'
+    keyword = search_keyword(request=request,
+                             products_obj=Product.objects.all())
+    form = SearchForm(initial={
+        'search_keyword': keyword['keyword']
+    })
+    page = my_paginator(request=request,
+                        objects=keyword['products'],
+                        items_in_page=3)
+    context = {
+        'products': page.object_list,
+        'form': form,
+        'page': page
+    }
+
+    return render(request, template, context=context)
+
+
+class InCategoryView(ListView):
+    """in category"""
+    template_name = 'in_category.html'
+    model = Product
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_pk = self.kwargs['pk']
+        keyword = search_keyword(request=self.request,
+                                 products_obj=self.model.objects.filter(category__super_category_id=category_pk))
+        form = SearchForm(initial={
+            'search_keyword': keyword['keyword'],
+        })
+        page = my_paginator(request=self.request,
+                            objects=keyword['products'],
+                            items_in_page=2)
+        context['sub_categories'] = SubCategory.objects.filter(super_category_id=category_pk)
+        context['category'] = Category.objects.get(pk=category_pk)
+        context['products'] = page.object_list
+        context['form'] = form
+        context['page'] = page
+        return context
+
+
+class InSubCategoryView(ListView):
+    """in sub category"""
+    template_name = 'in_sub_category.html'
+    model = Product
+    context_object_name = 'products'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        sub_category_pk = self.kwargs['pk']
+        keyword = search_keyword(request=self.request,
+                                 products_obj=self.model.objects.filter(category_id=sub_category_pk))
+        form = SearchForm(initial={
+            'search_keyword': keyword['keyword']
+            })
+        page = my_paginator(request=self.request,
+                            objects=keyword['products'],
+                            items_in_page=2)
+        category = SubCategory.objects.get(pk=sub_category_pk)
+        context['sub_category'] = category
+        context['sub_categories'] = SubCategory.objects.filter(super_category_id=category.super_category.pk)
+        context['products'] = page.object_list
+        context['form'] = form
+        context['page'] = page
+        return context
+
+
+class ProductDetailView(DetailView):
+    template_name = 'product_detail.html'
+    model = Product
+    context_object_name = 'product'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(self.model, pk=self.kwargs['pk'])
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        other_images = AdditionalImages.objects.filter(product_id=self.object.pk)
+        initial = {'product': self.object}
+        if self.request.user.is_authenticated:
+            initial['author'] = self.request.user.username
+        else:
+            initial['author'] = 'Anonymous user'
+        comments = Comment.objects.filter(product=self.object)
+        comment_page = my_paginator(request=self.request, objects=comments, items_in_page=4)
+        context['cart_form'] = CartAddProductForm()
+        context['page'] = comment_page
+        context['comments'] = comment_page.object_list
+        context['comment_form'] = UserCommentForm(initial=initial)
+        context['images'] = other_images
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(*args, **kwargs)
+        if 'add_comment' in self.request.POST:
+            form = UserCommentForm(self.request.POST)
+            if form.is_valid():
+                form.save()
+                context['form'] = form
+                messages.add_message(request=self.request, level=messages.SUCCESS,
+                                     message='Комментарий добавлен')
+            else:
+                context['form'] = UserCommentForm(request.POST)
+                messages.add_message(request=self.request, level=messages.WARNING,
+                                     message='Возникла ошибка при добавлении комментария')
+        return self.render_to_response(context)
